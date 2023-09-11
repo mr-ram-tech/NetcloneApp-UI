fbb4eddbfabecb84f138318825304b8f040e4096
rama <sramakrushna7@gmail.com>
2023-09-11 15:46:51 +0530

    fgy

---
+++ app/src/main/java/com/example/netclone/ExploreFragment.java
@@ -0,0 +1,31 @@
+package com.example.netclone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+
+public class ExploreFragment extends Fragment {
+TabLayout Tab;
+ViewPager viewPager;
+
+    public ExploreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_explore, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/netclone/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.netclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+BottomNavigationView btnView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnView = findViewById(R.id.btnView);
+
+        btnView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if(id==R.id.navRefine){
+                   loadFrag(new RefineFragment(),false);
+                }else{
+                    loadFrag(new ExploreFragment(),false);
+                }
+                return true;
+            }
+        });
+        btnView.setSelectedItemId(R.id.navRefine);
+    }
+
+    public void loadFrag(Fragment fragment, boolean flag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (flag)
+            ft.add(R.id.container, fragment);
+        else
+            ft.replace(R.id.container, fragment);
+        ft.commit();
+    }
+}
